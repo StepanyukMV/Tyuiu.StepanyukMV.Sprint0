3588dd44c30d179b45dc5046c97465c765e9179c
StepanyukMV <stepanyukmv@outlook.com>
2024-09-19 18:06:05 +0500

    Task1

---
+++ Tyuiu.StepanyukMV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿    namespace Tyuiu.StepanyukMV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Михаил";
+            int age = 21;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
