c4721c02859ecd1063c0cb5d1f16ff48e7c2f307
StepanyukMV <stepanyukmv@outlook.com>
2024-09-19 21:53:30 +0500

    Task5

---
+++ Tyuiu.Stepanyuk.MV.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+using Tyuiu.StepanyukMV.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.StepanyukMV.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionlValiid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(5, 10));
+        }
+
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+
+    }
+}
+
